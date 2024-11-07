ef94fbb99a8d19c3b7e92b4e60f2208ebfee643e
Ethan Havemann <echavemann@gmail.com>
2024-11-06 20:09:23 -0600

    switch comments

--- src/cppsrc/test/test_feature_pct_buy_trades.cpp
+++ src/cppsrc/test/test_feature_pct_buy_trades.cpp
@@ -1,9 +1,9 @@
-/* #include "gtest/gtest.h" */
-/**/
-/* TEST(FeatureTests, PctBuyTest) */
-/* { */
-/*     PercentBuyFeature ptf(); */
-/*     EXPECT_EQ(ptf.compute_feature({{1, 1, false}}), 0); */
-/*     EXPECT_EQ(ptf.compute_feature({{1, 1, false}, {1, 1, true}}), 0.5); */
-/*     EXPECT_EQ(ptf.compute_feature({{1, 1, true}}), 1); */
-/* } */
+#include "gtest/gtest.h"
+
+// TEST(FeatureTests, PctBuyTest)
+// {
+//     PercentBuyFeature ptf();
+//     EXPECT_EQ(ptf.compute_feature({{1, 1, false}}), 0);
+//     EXPECT_EQ(ptf.compute_feature({{1, 1, false}, {1, 1, true}}), 0.5);
+//     EXPECT_EQ(ptf.compute_feature({{1, 1, true}}), 1);
+// }
--- src/cppsrc/test/test_feature_sum_5tick_volume.cpp
+++ src/cppsrc/test/test_feature_sum_5tick_volume.cpp
@@ -1,13 +1,13 @@
 #include "gtest/gtest.h"
 
-/* TEST(FeatureTests, TestSum5TickVolume) */
-/* { */
-/*     FiveTickVolumeFeature vf(); */
-/*     EXPECT_EQ(vf.compute_feature({ { 1, 1, false } }), 1); */
-/*     EXPECT_EQ(vf.compute_feature({ { 1, 1, false } }), 2); */
-/*     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 4); */
-/*     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 6); */
-/*     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 8); */
-/*     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 9); */
-/*     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 10); */
-/* } */
+// TEST(FeatureTests, TestSum5TickVolume)
+// {
+//     FiveTickVolumeFeature vf();
+//     EXPECT_EQ(vf.compute_feature({ { 1, 1, false } }), 1);
+//     EXPECT_EQ(vf.compute_feature({ { 1, 1, false } }), 2);
+//     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 4);
+//     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 6);
+//     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 8);
+//     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 9);
+//     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 10);
+// }
