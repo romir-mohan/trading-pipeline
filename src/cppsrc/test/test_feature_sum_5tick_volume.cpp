b680e983b17229c5b3e733098b07871e6bfa488f
Ethan Havemann <echavemann@gmail.com>
2024-11-07 18:44:12 -0600

    improve clarity

--- src/cppsrc/test/test_feature_sum_5tick_volume.cpp
+++ src/cppsrc/test/test_feature_sum_5tick_volume.cpp
@@ -3,11 +3,11 @@
 // TEST(FeatureTests, TestSum5TickVolume)
 // {
 //     FiveTickVolumeFeature vf();
-//     EXPECT_EQ(vf.compute_feature({ { 1, 1, false } }), 1);
+//     EXPECT_EQ(vf.compute_feature({ { 2, 1, false } }), 1);
 //     EXPECT_EQ(vf.compute_feature({ { 1, 1, false } }), 2);
 //     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 4);
 //     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 6);
-//     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 8);
+//     EXPECT_EQ(vf.compute_feature({ { 2, 1, false }, { 1, 1, true } }), 8);
 //     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 9);
-//     EXPECT_EQ(vf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 10);
+//     EXPECT_EQ(vf.compute_feature({ { 2, 1, false }, { 1, 1, true } }), 10);
 // }
