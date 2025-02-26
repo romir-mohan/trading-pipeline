03262f4b4488182c806debe6c3076b2f660e3bc4
Romir Mohan <rmohan@black.tailf7f8d.ts.net>
2025-02-26 10:38:43 -0600

    fixed floating point issue

--- src/cppsrc/test/test_feature_pct_sell_trades.cpp
+++ src/cppsrc/test/test_feature_pct_sell_trades.cpp
@@ -6,5 +6,5 @@ TEST(FeatureTests, PctSellTest)
     intproj::PercentSellFeature psf;
     EXPECT_EQ(psf.compute_feature({{1, 1, false}}), 1);
     EXPECT_EQ(psf.compute_feature({{1, 1, false}, {1, 1, true}}), 0.5);
-    EXPECT_EQ(psf.compute_feature({{1, 1, false}, {1, 1, true}, {1, 2, false}}), 0.67);
+    EXPECT_EQ(psf.compute_feature({{1, 1, false}, {1, 1, true}, {1, 2, false}}), 0.666666687);
 }
