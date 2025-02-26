92729181a43b791722cc5c16b0312654cf14d796
Romir Mohan <rmohan@black.tailf7f8d.ts.net>
2025-02-26 10:43:20 -0600

    fixed estimation

--- src/cppsrc/test/test_feature_pct_sell_trades.cpp
+++ src/cppsrc/test/test_feature_pct_sell_trades.cpp
@@ -6,5 +6,5 @@ TEST(FeatureTests, PctSellTest)
     intproj::PercentSellFeature psf;
     EXPECT_EQ(psf.compute_feature({{1, 1, false}}), 1);
     EXPECT_EQ(psf.compute_feature({{1, 1, false}, {1, 1, true}}), 0.5);
-    EXPECT_NEAR(psf.compute_feature({{1, 1, false}, {1, 1, true}, {1, 2, false}}), 0.67, 1e-6);
+    EXPECT_NEAR(psf.compute_feature({{1, 1, false}, {1, 1, true}, {1, 2, false}}), 0.66666667, 1e-6);
 }
