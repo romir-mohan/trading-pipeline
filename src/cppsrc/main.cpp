390446858e44f6ece58a46c4935ca8de9843a196
Romir Mohan <rmohan@black.tailf7f8d.ts.net>
2025-02-27 14:30:11 -0600

    fixed main.cpp

--- src/cppsrc/main.cpp
+++ src/cppsrc/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <pybind11/pybind11.h>
 
+#include <pybind11/stl.h>
+
+
 int main()
 {
     std::cout << "hello\n";
@@ -14,4 +17,11 @@ int add(int a, int b)
 PYBIND11_MODULE(intern, m)
 {
     m.def("add", &add, "Adds two numbers");
+}
+
+PYBIND11_MODULE(intern, m)
+{
+    pybind11::class_<intproj::DataClient>(m, "DataClient")
+      .def(pybind11::init<>())
+      .def("tick", &intproj::DataClient::tick);
 }
