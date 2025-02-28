d81d10fdb29d21bfcf55c9b97143a5379a13e7e5
Romir Mohan <rmohan@black.tailf7f8d.ts.net>
2025-02-28 16:10:39 -0600

    added eval

---
+++ src/pysrc/model_eval.py
@@ -0,0 +1,21 @@
+from pysrc.data_client import DataClient
+from pysrc.model import Model
+from time import sleep
+
+client = DataClient()
+model = Model()
+counter = 1
+
+while counter <= 75:
+    print("Tick #: " + str(counter))
+
+    data = client.tick()
+    model.predict(data)
+
+    for waitCount in range(60):
+        print(".", end="", flush=True)
+        sleep(1)
+        waitCount += 1
+    print("\n")
+
+    counter += 1
