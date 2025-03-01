85a403f6a6b67e57b1db18918be8dfd890adf56e
Romir Mohan <rmohan@black.tailf7f8d.ts.net>
2025-02-28 20:28:02 -0600

    fixed conan and cmake

--- conanfile.py
+++ conanfile.py
@@ -9,6 +9,8 @@ class ExampleRecipe(ConanFile):
     def requirements(self):
         self.requires("gtest/1.15.0")
         self.requires("pybind11/2.13.6")
+        self.requires("cpr/1.11.1")
+        self.requires("nlohmann_json/3.11.3")
 
     def layout(self):
         cmake_layout(self)
