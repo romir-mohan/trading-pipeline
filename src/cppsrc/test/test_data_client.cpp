db060d768b009a6a19d86d40ea66bf2acb3da00f
Romir Mohan <rmohan@black.tailf7f8d.ts.net>
2025-02-28 23:21:38 -0600

    added data client tests

---
+++ src/cppsrc/test/test_data_client.cpp
@@ -0,0 +1,48 @@
+#include "/home/runner/work/intern-project/intern-project/src/cppsrc/data/data_client.hpp"
+#include "nlohmann/json.hpp"
+#include "gtest/gtest.h"
+
+#include <string>
+
+std::string sample_json_unit = R"([
+    {
+        "timestamp": 1,
+        "timestampms": 1000,
+        "tid": 1,
+        "price": "101",
+        "amount": "30",
+        "exchange": "gemini",
+        "type": "buy",
+    },
+    {
+        "timestamp": 2,
+        "timestampms": 2000,
+        "tid": 2,
+        "price": "202",
+        "amount": "20",
+        "exchange": "gemini",
+        "type": "buy",
+    },
+    {
+        "timestamp": 3,
+        "timestampms": 3000,
+        "tid": 3,
+        "price": "303.3",
+        "amount": "33.3",
+        "exchange": "gemini",
+        "type": "sell",
+    },
+])";
+
+TEST(DataClientTests, ParseMessageTest)
+{
+    nlohmann::json j = nlohmann::json::parse(sample_json_unit);
+    intproj::DataClient data_client;
+
+    std::vector<std::tuple<float, float, bool>> parsed = data_client.parse_message(j);
+
+    EXPECT_EQ(parsed.size(), 3);
+    EXPECT_EQ(std::get<0>(parsed[0]), 101f);
+    EXPECT_EQ(std::get<1>(parsed[0]), 30f);
+    EXPECT_TRUE(std::get<2>(parsed[0]));
+}
